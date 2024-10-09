08a6ec4b2cafe3cf91fe0d63793ddb79bc07a7e1
Shakila Kamalasena <104765058+shakilakamalasena@users.noreply.github.com>
2024-10-09 16:57:11 +0530

    Q1 Added

---
+++ Assignment05/Ques01.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+    <!-- <form action="formaction1.php" method="get"> -->
+    <form action="formaction1.php" method="post">
+        <p>Name: <input type="text" name="yourname" /><br />
+            E-mail: <input type="text" name="email" /></p>
+        <input type=submit value=submit>
+    </form>
+</body>
+
+</html>
